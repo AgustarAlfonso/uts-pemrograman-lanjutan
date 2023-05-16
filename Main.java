4c4d05b2d94672d820a832308ca4f8919c241ab6
Figgarofiggy <alfonsoagustar@gmail.com>
2023-05-16 19:13:40 +0700

    versi akhir

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        MencariAngkaUnik array1 = new MencariAngkaUnik();
+        array1.kondisiUnik();
     }
 }
--- MencariAngkaUnik.java
+++ MencariAngkaUnik.java
@@ -1,3 +1,65 @@
+import java.util.Scanner;
 public class MencariAngkaUnik {
-    
+    void kondisiUnik()
+    {
+        int[] array = new int[20];
+        Scanner input = new Scanner(System.in);
+        boolean kondisi = false;
+
+        for(int i=0;i<array.length;i++)
+        {
+            array[i] = input.nextInt();
+        }
+        for(int i=0;i<array.length;i++)
+        {
+            for(int j=i+1;j< array.length;j++){
+                if(array[i] == array[j])
+                {
+                    kondisi =  true;
+                }
+            }
+        }
+
+        if(kondisi==true)
+        {
+            System.out.println("True! ada angka yang sama!");
+            jumlahYangSama(array);
+        }
+        else
+        {
+            System.out.println("False! tidak ada angka yang sama");
+        }
+    }
+
+    private void jumlahYangSama(int[] array)
+    {
+        for (int i=0; i<array.length;i++)
+        {
+            boolean sudahdibandingkan = false;
+            for(int j=0;j<i;j++)
+            {
+                if (array[i] == array[j])
+                {
+                    sudahdibandingkan = true;
+                    break;
+                }
+            }
+            if (sudahdibandingkan==true)
+            {
+                continue;
+            }
+            int sama = 1;
+            for (int j = i+1;j<array.length;j++)
+            {
+                if (array[i] == array[j])
+                {
+                    sama++;
+                }
+            }
+            if (sama > 1)
+            {
+                System.out.println(array[i] + " ada " + sama);
+            }
+        }
+    }
 }
